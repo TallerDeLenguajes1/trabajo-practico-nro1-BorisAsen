1764f6c223cd90710213625708bf2d6f905e0191
BorisAsen <borisyasen@gmail.com>
2019-03-19 21:48:17 -0300

    Test Subida archivo.c

---
+++ Taller de Lenguajes 1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Taller.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("*** TALLER DE LENGUAJES 1 ***");
+	return 0;
+}
