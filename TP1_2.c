73690d284d44be7ef92c93d0d8f0f72acffc4cb8
BorisAsen <borisyasen@gmail.com>
2019-03-21 15:49:03 -0300

    Resolucion TP1

--- TP1_2.c
+++ TP1_2.c
@@ -13,68 +13,70 @@
 #include <math.h>
 
 int cuadrado(int num);
-void cuadrado_void(int num);
+void cuadrado_void(int num, int *sqr);
 void direccion_y_contenido(int num);
-void invertir(int a, int b);
-void ordenar(int a, int b);
+void invertir(int *a, int *b);
+void ordenar(int *c, int *d);
 
 int main(void) {
 
-	int num=3, a=7, b=2;
+	int num=3, a=7, b=2, sqr=0, c=11, d=5;
 
-	printf("I) El cuadrado de %i es %i\n",num,cuadrado(num));
-	cuadrado_void(num);
+//******************Apartado I)******************
+	printf("I) El cuadrado de %i es %i\n\n",num,cuadrado(num));
+
+//******************Apartado II)******************
+	cuadrado_void(num,&sqr);
+	printf("II) El cuadrado de %i es %i\n\n", num, sqr);
+
+//******************Apartado III)******************
 	direccion_y_contenido(num);
-	invertir(a, b);
-	ordenar(a, b);
+
+//******************Apartado IV)******************
+	printf("IV) Valores originales: a=%i, b=%i ", a, b);
+	invertir(&a,&b);
+	printf("| Valores invertidos: a=%i, b=%i\n\n", a, b);
+
+//******************Apartado IV)******************
+	printf("V) Valores originales: c=%i, d=%i ", c, d);
+	ordenar(&c, &d);
+	printf("| Valores ordenados: c=%i, d=%i\n\n", c, d);
 
 	return EXIT_SUCCESS;
-}
-
-
-int sumar(int a, int b){
-	return a+b;
 }
 
 int cuadrado(int num){
 	return pow(num,2);
 }
 
-void cuadrado_void(int num){
-	printf("II) El cuadrado de %i es %f\n", num, pow(num,2));
+void cuadrado_void(int num, int *sqr){
+	*sqr=pow(num,2);
+
 	return;
 }
 
 void direccion_y_contenido(int num){
-	printf("III) El contenido de la variable es %i y si direccion de memoria es %p\n", num, &num);
+	printf("III) El contenido de la variable es %i y si direccion de memoria es %p\n\n", num, &num);
 	return;
 }
 
-void invertir(int a, int b){
+void invertir(int *a, int *b){
 	int aux;
 
-	printf("IV) Valores originales: a=%i, b=%i ", a, b);
-	aux=a;
-	a=b;
-	b=aux;
-
-	printf("| Valores invertidos: a=%i, b=%i\n", a, b);
+	aux=*a;
+	*a=*b;
+	*b=aux;
 
 	return;
 }
 
-void ordenar(int a, int b){
+void ordenar(int *c, int *d){
 	int aux;
 
-	printf("V) Valores originales: a=%i, b=%i ", a, b);
-
-	if(a>b){
-		aux=a;
-		a=b;
-		b=aux;
-		printf("| Valores ordenados: a=%i, b=%i", a, b);
-	}else{
-		printf("| Valores ordenados: a=%i, b=%i", a, b);
+	if(*c>*d){
+		aux=*c;
+		*c=*d;
+		*d=aux;
 	}
 
 	return;
